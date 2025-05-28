02037dee61cc34f8dad67bc89c5fea6e7491ade7
santyago-05 <povedasantiago16@gmail.com>
2025-05-28 00:03:36 -0500

    feat: assesment

--- riesgocrediticio/src/main/java/com/rc_app/riesgocrediticio/Controller/AssessmentAdminController.java
+++ riesgocrediticio/src/main/java/com/rc_app/riesgocrediticio/Controller/AssessmentAdminController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 
+
 @RestController
 @RequestMapping("/api/admin/assessments")
 @PreAuthorize("hasRole('ADMIN')") // Protege todo el controlador
